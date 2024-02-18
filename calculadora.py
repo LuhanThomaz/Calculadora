bd44af2578c86bb59008b118ab2cb816e2941b53
LuhanThomaz <luhanthomaz@msn.com>
2024-02-18 16:29:32 -0300

    adição de mais comentários e explicações

--- calculadora.py
+++ calculadora.py
@@ -1,19 +1,28 @@
 import tkinter as tk
 from tkinter import ttk
-
+#Definição da classe calculadora que encapsula a lógica e a interface da calculadora
 class Calculadora:
-    
+    #método construtor
     def __init__(self, root):
+        #representa a instância da classe
         self.root = root
+        #root é um parâmetro que aguarda um objeto TK que é a janela principal da GUI, self.root  = root armazena o objeto TK passando como root na instância da classe
+        #self.root.title define o título da janela como calculadora
         self.root.title("Calculadora")
+
+
+        #Inicilizando uma variável como sttring vazia para armazenar a equação que será calculada
         self.texto_da_equacao = ""
 
 
+        # criação de um widget de texto que serve como display definindo as dimensões, bordas, cores e posicionamento.
         self.display = tk.Text(root, height=2, width=16, font=("Arial", 24), bd=0, bg="#f5f5f5")
         self.display.grid(row=0, column=0, columnspan=4, padx=5, pady=5)
 
         self.create_buttons()
 
+    
+    # cria os botões definindo as cores, estilo e dimensões.
     def create_buttons(self):
 
         button_color = "#e1e1e1"
@@ -37,7 +46,7 @@ class Calculadora:
             button.bind('<Button-1>', self.click)
 
 
-    
+    #método que define a lógica quando ocorre o click em algum  botão e captura o texto ou valor do botão pressionado.
     def click(self, event):
         text = event.widget.cget("text")
         if text == "=":
@@ -53,11 +62,12 @@ class Calculadora:
         else:
             self.display.insert(tk.END, text)
             self.texto_da_equacao += text
-    
+    #Função que Limpa o display e a variável self.texto_da_equacao
     def clear(self):
         self.display.delete(1.0, tk.END)
         self.texto_da_equacao = ""
 
+#criação da execução da calculadora
 if __name__ == "__main__":
     root = tk.Tk()
     root.configure(bg="#f5f5f5")
